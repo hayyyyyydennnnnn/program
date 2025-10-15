53f9d278b245f56af34a5f5580cb9953a618564a
hayyyyyydennnnnn <123008470+hayyyyyydennnnnn@users.noreply.github.com>
2025-10-15 10:57:42 -0400

    add charecter

---
+++ game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    
+    private Rigidbody playerRb;
+    public float jumpForce =10;
+    public float gravityModifier;
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifer;
+        
+    }
+    public bool isOnGround = true;
+    void Update()
+    {
+       if (Input.GetKeyDown(KeyCode.Space) && isOnGround) 
+       {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+       }
+    }
+    private void OnCollisionEnter(Collision collision )
+    {
+        isOnGround = true;
+    }
+}
