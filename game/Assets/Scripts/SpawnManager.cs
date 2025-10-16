bb3196dbc7a2e19d2b54619c42c4945141ab6b72
hayyyyyydennnnnn <123008470+hayyyyyydennnnnn@users.noreply.github.com>
2025-10-16 10:51:36 -0400

    finish spawn manager

--- game/Assets/Scripts/SpawnManager.cs
+++ game/Assets/Scripts/SpawnManager.cs
@@ -4,14 +4,21 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject obstaclePrefab;
     private Vector3 spawnPos = new Vector3(25, 0, 0);
+    private float startDelay = 2;
+    private float repeatRate = 2;
     void Start()
     {
-        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+
+    }
+    
+    void SpawnObstacle ()
+    {
+       Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
     }
 }
