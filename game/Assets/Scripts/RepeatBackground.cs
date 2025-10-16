fe49472545502d2bd55ce3f5d3de60fe9a119018
hayyyyyydennnnnn <123008470+hayyyyyydennnnnn@users.noreply.github.com>
2025-10-16 11:14:33 -0400

    add new game over trigger

--- game/Assets/Scripts/RepeatBackground.cs
+++ game/Assets/Scripts/RepeatBackground.cs
@@ -3,15 +3,17 @@ using UnityEngine;
 public class NewMonoBehaviourScript : MonoBehaviour
 {
     private Vector3 startPos;
+    private float repeatWidth;
     void Start()
     {
         startPos = transform.position;
+        repeatWidth = GetComponent<BoxCollider>().size.x / 2;
     }
 
   
     void Update()
     {
-        if (transform.position.x < startPos.x - 70)
+        if (transform.position.x < startPos.x - repeatWidth)
         {
             transform.position = startPos;
         }
