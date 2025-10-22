d6543d08ad38d7c7a7cb392e2caf31e3c4946d4a
hayyyyyydennnnnn <123008470+hayyyyyydennnnnn@users.noreply.github.com>
2025-10-22 11:17:35 -0400

    falling

--- game/Assets/Scripts/PlayerController.cs
+++ game/Assets/Scripts/PlayerController.cs
@@ -8,19 +8,23 @@ public class PlayerController : MonoBehaviour
     public float gravityModifier;
     public bool isOnGround = true;
     public bool gameOver = false;
+    private Animator playerAnim;
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
         
     }
     void Update()
     {
-       if (Input.GetKeyDown(KeyCode.Space) && isOnGround) 
-       {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
+        {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
-       }
+            playerAnim.SetTrigger("Jump_trig");
+        }
+       
     }
     private void OnCollisionEnter(Collision collision )
     {
@@ -31,6 +35,8 @@ public class PlayerController : MonoBehaviour
         {
             Debug.Log("Game Over");
             gameOver = true;
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
         }
     }
 }
