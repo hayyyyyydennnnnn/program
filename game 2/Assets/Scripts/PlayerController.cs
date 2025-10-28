06b56ff4ec5ab2ff48f135e8e031fde4d3f15d74
hayyyyyydennnnnn <123008470+hayyyyyydennnnnn@users.noreply.github.com>
2025-10-28 10:53:25 -0400

    Focal Point

--- game 2/Assets/Scripts/PlayerController.cs
+++ game 2/Assets/Scripts/PlayerController.cs
@@ -4,16 +4,17 @@ public class PlayerController : MonoBehaviour
 {
    private Rigidbody playerRb;
    public float speed = 5.0f;
+   private GameObject focalPoint;
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
-
+        focalPoint = GameObject.Find("Focal point");
     }
 
     
     void Update()
     {
         float forwardInput = Input.GetAxis("Vertical");
-        playerRb.AddForce(Vector3.forward * speed * forwardInput);
+        playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
     }
 }
