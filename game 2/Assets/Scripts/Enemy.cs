295daf2d79df8d0244ec939959e3c3f5f772e684
hayyyyyydennnnnn <123008470+hayyyyyydennnnnn@users.noreply.github.com>
2025-10-29 10:08:05 -0400

    enemy follows player

---
+++ game 2/Assets/Scripts/Enemy.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{   public float speed;
+    private Rigidbody enemyRb;
+    private GameObject player;
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+        player = GameObject.Find("Player");
+    }
+
+    void Update()
+    {
+        enemyRb.AddForce((player.transform.position - transform.position).normalized * speed);
+    }
+}
