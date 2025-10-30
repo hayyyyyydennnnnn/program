33d2ab491d9f036d3632b0c515ef6a29b6b4efac
hayyyyyydennnnnn <123008470+hayyyyyydennnnnn@users.noreply.github.com>
2025-10-30 10:24:11 -0400

    spawn random

---
+++ game 2/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{   
+    public GameObject enemyPrefab;
+    void Start()
+    {
+        Instantiate(enemyPrefab, new Vector3(0, 0, 6), enemyPrefab.transform.rotation);
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
