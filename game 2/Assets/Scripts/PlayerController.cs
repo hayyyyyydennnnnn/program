b9ba466ed1f23b2f36e67ece4c0fdeab0e14c7a2
hayyyyyydennnnnn <123008470+hayyyyyydennnnnn@users.noreply.github.com>
2025-10-31 11:09:04 -0400

    Apply extra knockback with powerup

--- game 2/Assets/Scripts/PlayerController.cs
+++ game 2/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
    public float speed = 5.0f;
    private GameObject focalPoint;
    public bool hasPowerup = false;
+   private float powerupStrength = 15.0f;
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
@@ -31,7 +32,11 @@ public class PlayerController : MonoBehaviour
     {
         if(collision.gameObject.CompareTag("Enemy") && hasPowerup)
         {
+            Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position);
+
             Debug.Log("Collided with: " + collision.gameObject.name + " with powerup set to " + hasPowerup);
+            enemyRigidbody.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
         }
     }
 }
